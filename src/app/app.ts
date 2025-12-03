33e84fe36775631c7c522d4c57a516a5a6d9df3c
Daniel Cubides <daniandrescubidesh@gmail.com>
2025-12-02 23:48:10 -0500

    Creación del servicio de comunicación entre componentes y los componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Componente1 } from './componente1';
+import { Componente2 } from './componente2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Componente1, Componente2],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
